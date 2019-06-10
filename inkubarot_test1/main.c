9f92c5eb9c30264b08abf6cbe79b9b50b4f54bba
dominikk787 <42272142+dominikk787@users.noreply.github.com>
2019-06-10 15:53:54 +0200

    Initial commit

---
+++ inkubarot_test1/main.c
@@ -0,0 +1,19 @@
+/*
+ * inkubarot_test1.c
+ *
+ * Created: 2019-06-09 15:09:15
+ * Author : programowanie
+ */
+
+#include <avr/io.h>
+#include <usart.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1)
+    {
+    }
+}
+
